4504bd227516c0d9e184bacbf300025832d84ad5
Cody Sobas <codysobas@Mac.home.local>
2026-04-17 12:11:22 -0500

    fix: persist supabase session to localStorage

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://qxypaepvmtmkhbssedki.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4eXBhZXB2bXRta2hic3NlZGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzE0MTcsImV4cCI6MjA4ODgwNzQxN30.kCZjM-wR8GbRC4K2A8-r1EBVgkzRD1shx3Vl3EEyELE'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    storageKey: 'sb-qxypaepvmtmkhbssedki-auth-token',
+    storage: window.localStorage,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+})
