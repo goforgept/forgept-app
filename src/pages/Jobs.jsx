7b031274dc6ebe28911c863934dc50d99f39616d
Cody Sobas <codysobas@Mac.home.local>
2026-08-19 13:17:56 -0500

    update jobs with statuses

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -113,7 +113,7 @@ export default function Jobs({ isAdmin, featureProposals = true, featureCRM = fa
     <div className="flex min-h-screen bg-fp-inset">
       <Sidebar isAdmin={isAdmin} featureProposals={featureProposals} featureCRM={featureCRM} featurePurchaseOrders={featurePurchaseOrders} featureInvoices={featureInvoices} featureInventory={featureInventory} isSalesManager={isSalesManager} isPM={isPM} isTechnician={isTechnician} />
 
-      <div className="flex-1 p-6 space-y-6">
+      <div className="flex-1 p-6 space-y-6 min-w-0">
         {/* Header */}
         <div className="flex justify-between items-center">
           <div>
