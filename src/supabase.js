bbb1fcbeabde00434425d193905caf9206cdcbbb
Cody Sobas <codysobas@Mac.home.local>
2026-09-01 11:15:18 -0500

    migrate to new Supabase publishable API key

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qxypaepvmtmkhbssedki.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4eXBhZXB2bXRta2hic3NlZGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzE0MTcsImV4cCI6MjA4ODgwNzQxN30.kCZjM-wR8GbRC4K2A8-r1EBVgkzRD1shx3Vl3EEyELE'
+const supabaseKey = 'sb_publishable__NqeZGEO3Uh3JR5fK3pk8w_UBngpGZS'
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
